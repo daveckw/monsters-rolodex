7a63acefc179692d3b40a4ccb811c811eb53b663
daveckw <daveckw@gmail.com>
2019-10-24 19:52:26 +0800

    Updates

---
+++ precache-manifest.af91c69d1de06b383ca77d1db3bbb4be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b652e4c2a69807b1b65e741d32049fb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "43bddeba0a5b3da0ff4d",
+    "url": "/monsters-rolodex/static/css/main.2a812357.chunk.css"
+  },
+  {
+    "revision": "4e212bf9b048ba3d9cd6",
+    "url": "/monsters-rolodex/static/js/2.1b77c95c.chunk.js"
+  },
+  {
+    "revision": "43bddeba0a5b3da0ff4d",
+    "url": "/monsters-rolodex/static/js/main.f6ab9fb4.chunk.js"
+  },
+  {
+    "revision": "c3c4968e073d79b7868a",
+    "url": "/monsters-rolodex/static/js/runtime-main.c579c10c.js"
+  }
+]);
